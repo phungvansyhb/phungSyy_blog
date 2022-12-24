93b0a8c11f44fa594e92375c0fad2e490fb6c507
sybk99 <phungvansyhb@gmail.com>
2022-12-24 14:21:22 +0700

    add config theme

---
+++ assets/icons/index.tsx
@@ -0,0 +1,6 @@
+import BlogIconHeader from "./BlogIconHeader";
+import ProjectIconHeader from "./ProjectIconHeader";
+import AboutIconHeader from "./AboutIconHeader";
+import LighThemeIcon from "./LighThemeIcon";
+import DarkThemeIcon from "./DarkThemeIcon";
+export { BlogIconHeader, ProjectIconHeader, AboutIconHeader, LighThemeIcon, DarkThemeIcon };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Courier_Prime } from "@next/font/google";
+
+const courierPrime = Courier_Prime({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main className={courierPrime.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
